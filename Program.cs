ea3100609c8d4c6eb3eda84b9069b02666be0825
Krushna Magar <111754254+krushnamagar009@users.noreply.github.com>
2022-09-08 15:48:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using MovieAPI_Context.Model;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MovieContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Ecommerce"));
+});
+
+//builder.Services.AddDbContext<MovieContext>(options => options.UseInMemoryDatabase("Ecommerce"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
